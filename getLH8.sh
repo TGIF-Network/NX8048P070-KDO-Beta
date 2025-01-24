d5f03d41d8702b909702ecf308ac360ddd992252
root <root@pi-star>
2025-01-24 18:45:21 -0500

    update

--- getLH8.sh
+++ getLH8.sh
@@ -27,15 +27,20 @@ function domode2
 line3=""
 call1=""
 name=""
+line=list1
 while read -r line
 do
-	call=$(echo "$line" | cut -d' ' -f3)
+	call=$(echo "$line" | cut -d' ' -f14)
 
-	tm=$(echo "$line" | cut -d' ' -f2)
-	dt=$(echo "$line" | cut -d' ' -f1)
-	pl=$(echo "$line" | cut -d' ' -f5)
+	tm=$(echo "$line" | cut -d' ' -f3)
+	dt=$(echo "$line" | cut -d' ' -f2)
+	tm=${tm:0:5}
+
+	pl=$(echo "$line" | cut -d' ' -f20)
+	tg=$(echo "$line" | cut -d' ' -f17)
 
 	echo "Add Call: $call" >> /home/pi-star/lh2_start.txt
+
 	dataline=$(sudo sed -n "/$call/p" /usr/local/etc/stripped.csv)
        	did=$(echo "$dataline" | cut -d',' -f1 | head -1)
         call1=$(echo "$dataline" | cut -d',' -f2 | head -1)
@@ -58,10 +63,12 @@ do
 	line6=$(echo "$line3" | tr -d "\n")
 
 	list5+="$line6"
-done <<< "$list9"
+done <<< "$list1"
 
 var="${list5:0:400}"
 #echo "${var}"
+sudo mount -o remount,rw /
+
 echo "${var}" >> ./lh2_start.txt
 
 }
@@ -72,22 +79,31 @@ echo "${var}" >> ./lh2_start.txt
 ######################################
 
 f1=$(ls -tr /var/log/pi-star/MMDVM* | tail -1)
-list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sed 's/,//g' | sort -r -k14,14 -k2,3)
+#list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | sort -k1 | sed 's/,//g' | sort -r -k14,14 -k2,3)
+list1=$(sudo sed -n '/received network end of voice transmission from/p' $f1 | tail -n 50 | sort -r -k15 -u |tail -n8)
+list1=$(tail -n 30 /var/log/pi-star/MMDVM* | grep 'end of voice transmission from' | sort -u -k14 | sort -k3 | tail -n8 | sort -k2)
+list1=$(cat /var/log/pi-star/MMDVM* | grep "end of voice transmission from" | tail -16 | sort -u -k15 | sort -k3 | tail -n8)
 
-#echo "$list1"
-list3=$(echo "$list1" | awk '$14!=savestr {print substr($2,6,5),substr($3,0,6),$14,$17,$6,$18,$20; savestr=$14}' | sort -r -k1,2 | tail -8)
+list1=$(echo "$list1" | tail -n8)
 
+#list3=$(echo "$list1" | awk '$14!=savestr {print substr($2,6,5),substr($3,0,6),$14,$17,$6,$18,$20; savestr=$14}' | sort -u -r -k1,2 | tail -8)
+#echo "$list3"
 
-lcnt=$(echo "$list3" | wc -l)
-if [ $lcnt -lt 20 ]; then
-	list3+=$list4
-fi
+#list3=$(echo "$list1" | awk '$14!=savestr {print substr($2,6,5),substr($3,0,6),$14,$17,$6,$18,$20; savestr=$14}' | sort -u -r -k2,2 | tail -8)
+#echo "$list3"
+#list4=$(echo "$list3" | sort -u -k2)
+#echo  "4 - $list4"
+#lcnt=$(echo "$list3" | wc -l)
+#if [ $lcnt -lt 20 ]; then
+#	list3+=$list4
+#fi
 #  echo "Test1234 | Test 2345 | TestTest\r |12345678"
 
 #echo "$list3"
 
 #echo "$list3" | sed -n '1,8p;9q' |  awk '{printf "%5s %5s %7s %6s %1s %4s %-4s|\n", $1, $2, $3, $4, $5, $6, $7}' | tr -d "\n | tail -8"
-list9=$(echo "$list3" | sed -n '1,6p;7q' | awk '{print $1, $2, $3, $4}' )
+#list9=$(echo "$list1" | sed -n '1,6p;7q' | awk '{print $1, $2, $3, $4}' )
+
 	domode2
 
 sudo mount -o remount,ro /
