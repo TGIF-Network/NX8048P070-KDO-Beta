31495c41448c276cf21af2ea81fb18c886ea17bd
root <root@pi-star>
2025-02-27 14:48:25 -0500

    uodate

--- checkfortft.sh
+++ checkfortft.sh
@@ -16,18 +16,23 @@ F1=$(date -r  /usr/local/etc/NX4832K035.tft | tr '  ' ' ')
 #m4=$(cat /proc/meminfo |head -3 |head -1 | tr -d ' ')
 
 #echo "$F1"
-dt0=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f2)
-dt1=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f3)
-dt2=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f4)
-dt3=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f7)
+#dt0=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f2)
+#dt1=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f3)
+#dt2=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f4)
+#dt3=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f7)
 dt4=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f5)
-dt="$dt3 $dt2  $dt1"
+#dt="$dt3 $dt2  $dt1"
+
+dtt=$(stat /usr/local/etc/NX4832K035.tft | grep Birth | cut -d '.' -f1 | cut -d ' ' -f3,4)
+#echo "$dtt"
+
  if [ ! -f "$FILE" ]; then
         # Copy failed
       echo "No TFT File Available to Flash - Try Again"
   else
 #   echo "NX4832K035.tft \r$dt1 $dt2 $dt3"
-   echo "NX4832K035.tft File Found!\r$dt0 $dt1 $dt2 $dt3$dt4"
+#   echo "NX4832K035.tft File Found!\r$dt0 $dt1 $dt2 $dt3$dt4"
+   echo "NX4832K035.tft File Found!\r $dtt $dt4"
 
  fi
 
