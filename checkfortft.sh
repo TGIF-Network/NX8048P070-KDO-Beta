4393f905fec416bc1abb88d6262d672e54575946
root <root@pi-star>
2025-03-31 21:06:18 -0400

    update

--- checkfortft.sh
+++ checkfortft.sh
@@ -10,20 +10,15 @@ set -o errexit
 set -o pipefail
 #Sun  9 Feb 10:13:59 EST 2025
 
- FILE=/usr/local/etc/NX4832K035.tft
+ FILE=/usr/local/etc/NX8048P070.tft
 #F1=$(date -r  /usr/local/etc/NX4832K035.tft | sed 's/ /-/g')
-F1=$(date -r  /usr/local/etc/NX4832K035.tft | tr '  ' ' ')
-#m4=$(cat /proc/meminfo |head -3 |head -1 | tr -d ' ')
+F1=$(date -r  /usr/local/etc/NX8048P070.tft | tr '  ' ' ')
 
-#echo "$F1"
-#dt0=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f2)
-#dt1=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f3)
-#dt2=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f4)
-#dt3=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f7)
-dt4=$(date -r /usr/local/etc/NX4832K035.tft | cut -d " " -f5)
+dt4=$(date -r /usr/local/etc/NX8048P070.tft | cut -d " " -f5)
+
 #dt="$dt3 $dt2  $dt1"
 
-dtt=$(stat /usr/local/etc/NX4832K035.tft | grep Birth | cut -d '.' -f1 | cut -d ' ' -f3,4)
+dtt=$(stat /usr/local/etc/NX8048P070.tft | grep Birth | cut -d '.' -f1 | cut -d ' ' -f3,4)
 #echo "$dtt"
 
 tag=$(git -C /home/pi-star/Nextion_Temp/ tag)
@@ -31,10 +26,7 @@ tag=$(git -C /home/pi-star/Nextion_Temp/ tag)
         # Copy failed
       echo "No TFT File Available to Flash - Try Again"
   else
-#   echo "NX4832K035.tft \r$dt1 $dt2 $dt3"
-#   echo "NX4832K035.tft File Found!\r$dt0 $dt1 $dt2 $dt3$dt4"
-   echo "NX4832K035.tft File Found!\r Repo Tag: $tag"
-#   echo "NX4832K035.tft File Found!\r $dt4"
+   echo "NX7048P070.tft File Found!\r Repo Tag: $tag"
 
  fi
 
